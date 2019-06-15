f0dd45acda15b41d6bc26f92f2e900719168973f
gwynere <janpardak@gmail.com>
2019-06-16 00:19:46 +0200

    Updates

---
+++ precache-manifest.7e1f843b57833e188c7b8dfe7bc1d123.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b9aab732f8fa904782dc4af3f8b494c",
+    "url": "/recipe-app-with-react/index.html"
+  },
+  {
+    "revision": "57a011809073acc643bc",
+    "url": "/recipe-app-with-react/static/css/2.49226c33.chunk.css"
+  },
+  {
+    "revision": "22b0b1716970978be202",
+    "url": "/recipe-app-with-react/static/css/main.f341b404.chunk.css"
+  },
+  {
+    "revision": "57a011809073acc643bc",
+    "url": "/recipe-app-with-react/static/js/2.54103344.chunk.js"
+  },
+  {
+    "revision": "22b0b1716970978be202",
+    "url": "/recipe-app-with-react/static/js/main.60756cef.chunk.js"
+  },
+  {
+    "revision": "56e7b099f82cceff6d61",
+    "url": "/recipe-app-with-react/static/js/runtime~main.78b5c6d1.js"
+  }
+]);
